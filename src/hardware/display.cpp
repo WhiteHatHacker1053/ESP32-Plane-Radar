1b195a8f76b932ea73ee74c51dbe4e5ffe27054a
WhiteHatHacker1053 <grifmor4@outlook.com>
2026-06-05 20:49:59 -0700

    Update display driver for WeAct 2.9" e-ink display with GxEPD2

--- src/hardware/display.cpp
+++ src/hardware/display.cpp
@@ -1,13 +1,20 @@
 #include "hardware/display.h"
 
 #include "hardware/display_font.h"
+#include <GxEPD2_BW.h>
+#include <GxEPD2_display_selection_new_style.h>
 
-LGFX tft;
+// WeAct 2.9" e-ink display: 128x296, black/white
+GxEPD2_BW<GxEPD2_290_GDEY029T94, GxEPD2_290_GDEY029T94::HEIGHT> display(
+    GxEPD2_290_GDEY029T94(
+        /*CS=*/ GPIO_NUM_5,
+        /*DC=*/ GPIO_NUM_33,
+        /*RST=*/ GPIO_NUM_19,
+        /*BUSY=*/ GPIO_NUM_26));
 
 void displayInit() {
-  tft.init();
-  tft.setRotation(0);
-  tft.setBrightness(255);
-  tft.setTextWrap(false);
+  display.init(115200, true, 2, false);
+  display.setRotation(0);
+  display.setTextWrap(false);
   displayFontInit();
 }
